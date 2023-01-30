8ab0dac9bb7a017038442a17f534ca553f3af608
luongthanhthang <thanhthang2762000@gmail.com>
2023-01-30 15:29:44 +0700

    nop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,74 +1,50 @@
-// function getCookieOnload() {
-//   let check = decodeURIComponent(document.cookie);
-//   console.log(check);
-//   let ca = check.split("=");
-//   console.log(ca[1]);
-//   changeSidebar(!ca[1]);
-// }
-
 function changeSidebar() {
-  // document.cookie = "sidebar=" + check;
-  if (document.getElementById("sidebar").className == "sidebar") {
-    document.getElementsByClassName("content")[0].style.padding = "0 0 0 60px";
-    document.getElementById("sidebar").className = "sidebar-small";
+  document.getElementById("sidebar").classList.toggle("sidebar-small");
+  if (document.getElementById("sidebar").className === "sidebar-small") {
+    document.getElementsByClassName("content")[0].style.paddingLeft = "60px";
   } else {
-    document.getElementById("sidebar").className = "sidebar";
-    document.getElementsByClassName("content")[0].style.padding = "0 0 0 200px";
+    document.getElementsByClassName("content")[0].style.paddingLeft = "200px";
+  }
+  console.log(document.getElementById("sidebar").className)
+  setCookie('sidebarChange', document.getElementById("sidebar").className);
+}
+let subMenu = document.querySelectorAll(".nav-links li");
+subMenu.forEach((item) => {
+  item.onclick = () => {
+    item.classList.toggle("is-active");
+  };
+});
+
+function getCookieSidebar(sidebarChange) {
+  document.getElementById("sidebar").className = getCookie(sidebarChange);
+  if (document.getElementById("sidebar").className === "sidebar-small") {
+    document.getElementsByClassName("content")[0].style.paddingLeft = "60px";
+  } else {
+    document.getElementsByClassName("content")[0].style.paddingLeft = "200px";
   }
 }
 
-function displaySubMenu(titleMenu) {
-  switch (titleMenu) {
-    case "OKRs":
-      if (
-        document.getElementsByClassName("OKRs-sub-menu")[0].style.display ==
-        "block"
-      ) {
-        document.getElementsByClassName("OKRs-sub-menu")[0].style.display =
-          "none";
-      } else {
-        document.getElementsByClassName("OKRs-sub-menu")[0].style.display =
-          "block";
-      }
-      break;
+function setCookie(cname, cvalue) {
+  console.log(cvalue);
+  const d = new Date();
+  d.setMonth(d.getMonth() + 1);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-    case "course":
-      if (
-        document.getElementsByClassName("course-sub-menu")[0].style.display ==
-        "block"
-      ) {
-        document.getElementsByClassName("course-sub-menu")[0].style.display =
-          "none";
-      } else {
-        document.getElementsByClassName("course-sub-menu")[0].style.display =
-          "block";
-      }
-      break;
-
-    case "gift":
-      if (
-        document.getElementsByClassName("gift-sub-menu")[0].style.display ==
-        "block"
-      ) {
-        document.getElementsByClassName("gift-sub-menu")[0].style.display =
-          "none";
-      } else {
-        document.getElementsByClassName("gift-sub-menu")[0].style.display =
-          "block";
-      }
-      break;
-
-    case "CFRs":
-      if (
-        document.getElementsByClassName("CFRs-sub-menu")[0].style.display ==
-        "block"
-      ) {
-        document.getElementsByClassName("CFRs-sub-menu")[0].style.display =
-          "none";
-      } else {
-        document.getElementsByClassName("CFRs-sub-menu")[0].style.display =
-          "block";
-      }
-      break;
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie); // lấy cookie
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1); // xoá hết khoảng trống đứng đầu
+    }
+    if (c.indexOf(name) == 0) {
+      // keyName đứng đầu
+      return c.substring(name.length, c.length); // lấy giá trị cookie string.substring(start, end)
+    }
   }
+  return "";
 }
